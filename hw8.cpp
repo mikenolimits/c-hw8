e5a1276bdfe704afbb7e77198bc7b371a8cfd3bd
mikenolimits <mkantor100@qc.cuny.edu>
2014-11-24 14:17:24 -0500

    working

--- hw8.cpp
+++ hw8.cpp
@@ -3,25 +3,29 @@ using namespace std;
 
     void shiftRight(double x[], int y){
 
-    for(int z = 0; z <= (y - 1); z++){
-     
-       if(z == y){
-          x[0] = x[y];
-       }if(z != 0{
-         x[z] = x[y + 1];
-      }
-                    
+    double temp[y];
+    for (int i = 0; i <= (y-1); i++){
+       temp[i] = x[i];
+    }
+   for(int z = 0; z <= (y-1); z++){
+       temp[z] = x[z - 1];
+       if(z == (y - 1)){
+          temp[0] = x[z];
+       }
+       
     }
 
-     for(int i = 0; i <= y; i++){
-       cout<<x[i];
-     }
+    for( int p = 0; p <= (y-1); p++){
+       x[p] = temp[p];
+      cout<<x[p]<<endl;
+    }
 
-   }
+  } 
 
     int main(){
 
-     double x[5] = {0.0,1.1,2.1,3.1,4.4};
+     double x[5] = {0.5,1.1,2.1,3.1,4.4};
      int y = 5;   
+     shiftRight(x,y);
 
 } 
